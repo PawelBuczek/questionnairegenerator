924672dc92b8f8fe02d97f0dfdde23a32c295115
Pawel <pawel.buczek@email.com>
2023-07-07 17:25:49 +0200

    added tests

--- test_questionnaire.py
+++ test_questionnaire.py
@@ -1,44 +1,52 @@
 import pytest
-
-# there are probably other tests that we could think of here, but these 3 will be good for our learning purposes.
+import b_add_indicators
+import a_generate_questionnaire
 
 # to run all these test, simply type  "pytest"  in the console (may require to kill it and open again)
-# to run only specific test, for example:  pytest -k 'test_indicators_are_correctly_added'
-
 
 def test_indicators_are_correctly_added():
-    pass  # delete this line after you've written this test
+    my_input = "Test\nX 1st line\nX 2nd line"  # preparing text as our input
+    my_desired_output = "Test\n:regional_indicator_a: 1st line\n:regional_indicator_b: 2nd line"  # preparing our desired output
+    
+    my_actual_output = b_add_indicators.add_indicators(my_input)  # running the method, passing our input as parameter, and saving it as output
+    
+    assert(my_desired_output == my_actual_output)  # asserting if our output is what we exptected
 
-    # purpose of this test is to check `add_indicators` method from file `b_add_indicators` (will require import).
-    # for this we don't really need to care about any text file.
-    # that's because this method takes in string and returns string.
-
-    # so we may just pass some text to this method, try for example: "Test\nX 1st line\nX 2nd line"
-    # and assert if what was returned is the same as expected, for example: "Test\n:regional_indicator_a: 1st line\n:regional_indicator_b: 2nd line"
+    # note: we could also do all of the above in one line (though it may be less clear) like below:
+    # assert("Test\n:regional_indicator_a: 1st line\n:regional_indicator_b: 2nd line" == b_add_indicators.add_indicators("Test\nX 1st line\nX 2nd line"))
 
 
 def test_questionnaire_is_generated_for_Jul2023():
-    pass  # delete this line after you've written this test
+    with open("test_Jul2023_questionnaire.txt", "r", encoding="utf-8") as f:
+        my_desired_output = f.read()  # reading our stored copy into variable
 
-    # this test can be for any month, Jul2023 is just an example. (Yes, checking one random month should should is sufficient for our needs.)
-    # first of all - try to run method `generate_questionnaire` from file `a_generate_questionnaire` with correct text for given month passed (will require import).
-    # then run this test and check manually if it works. Was file `questionnaire.txt` populated?
-    # after that is done, try to check if the file is ok.
+    my_date_in_text_format = "2023-07-01"  # date for our chosen month, could be any day of the month really
+    a_generate_questionnaire.generate_questionnaire(my_date_in_text_format)  # generating questionnaire for a given month
 
-    # this can be done in a few ways, but I would recommend copying generated file with a different name (something like `test_Jul2023_questionnaire.txt`).
-    # and then in this test asserting if newly generated file is the same as our saved copy (copy will not be modified, unless business requirements change).
-    # with this approach we just need to manually check generated file once, then test is asserting that no changes to the logic are made.
+    with open("questionnaire.txt", "r", encoding="utf-8") as f:
+        my_actual_output = f.read()  # reading created output into variable
+
+    assert(my_desired_output == my_actual_output)  # asserting if our output is what we exptected
 
 
 def test_questionnaire_generation_and_indicators_adding_work_together():
-    pass  # delete this line after you've written this test
+    my_date_in_text_format = "2031-12-01"
+    a_generate_questionnaire.generate_questionnaire(my_date_in_text_format)
 
-    # final test. Some would say this is not needed since we already checked pretty much everything, but for learning purposes let's roll with it.
-    # Here we need to first run `generate_questionnaire` from file `a_generate_questionnaire` with correct text passed as a parameter.
-    # then we need to modify `questionnaire.txt` file, for example by removing lines 10 through 20.
-    # then we run `add_indicators_to_file` method from file `b_add_indicators`.
-    # and finally we check if what was created is what was expected. Here, to make it a bit different from the previous test, let's change our tactic.
-    # instead of comparing txt file with a saved copy, lets make 3 assertions:
-    # - is `questionnaire.txt` file third line starting with ":regional_indicator_b"
-    # - is `questionnaire.txt` file first line NOT starting with ":regional_indicator"
-    # - is `questionnaire.txt` file NOT ending with an empty line (the last character is not '\n')
+    # removing the lines in the next 7 code lines, resource link: https://www.tutorialspoint.com/how-to-delete-specific-line-from-a-text-file-in-python
+    with open("questionnaire.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open("questionnaire.txt", "w", encoding="utf-8") as f:
+        for number, line in enumerate(lines):
+            if number < 10 or number > 20:
+                f.write(line)
+    
+    b_add_indicators.add_indicators_to_file()
+
+    with open("questionnaire.txt", "r", encoding="utf-8") as f:
+        my_output_lines = f.readlines()  # reading output as lines will make it easier for us to work with them
+
+    assert(my_output_lines[2].startswith(":regional_indicator_b"))
+    assert(not my_output_lines[0].startswith(":regional_indicator"))  # we can add 'not' to our assertions to make them easier
+    assert(not my_output_lines[-1].endswith("\n"))  # [-1] means last element of the list, last line of our file in this example
